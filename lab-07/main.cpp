4e5b7f7a0d125a54d22699a0998d1500cdb1b58b
davinbernier <davinlbernier@gmail.com>
2018-03-02 14:29:09 -0500

    Create new testMerge and testMergeSort
    Also removed my answers to merge and MergeSort that I accidentally left in there last commit.

--- lab-07/main.cpp
+++ lab-07/main.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <cstring>
 #include <random>
 #include <chrono>
+#include <algorithm>
 
-void testMerge(int n);
-void testMergeSort(int n);
-void raceMergeSorts(int n);
-void merge(int* A, int n, int mid);
-void MergeSort(int* A, int n);
+int* gen_list_unsorted(int n);
 
 void merge(int* A, int n, int mid) {
-    int* B = new int[n];
-    for (int i = 0; i < n; i++) {
-        B[i] = A[i];
-    }
-    int x = 0;
-    int y = mid;
-    int idx = 0;
-    while (x < mid && y < n) {
-        if (B[x] < B[y]) {
-            A[idx++] = B[x++];
-        } else {
-            A[idx++] = B[y++];
-        }
-    }
 
-    if (x < mid) {
-        y = x;
-        n = mid;
-    }
-
-    for (int i = y; i < n; i++) {
-        A[idx++] = B[i];
-    }
-
-    delete [] B;
 }
 
 void MergeSort(int* A, int n) {
-    if (n != 1) {
-        MergeSort(A, n/2);
-        int n2 = (n % 2 == 0) ? n/2 : n/2 + 1;
-        MergeSort(A + n/2, n2);
-        merge(A, n, n/2);
+
+}
+
+/* Compares int arrays of similar length n for equality */
+bool compareArrays(int* A, int* B, int n) {
+    bool success = true;
+    for (int i = 0; i < n; i++) {
+        if (A[i] != B[i]) {
+            success = false;
+            break;
+        }
     }
+    return success;
 }
 
 void customTest(int n) {
@@ -54,115 +32,63 @@ void customTest(int n) {
 
 }
 
-void testMerge() {
-    //Test the merge function
-    std::ifstream mergeIn("mergeTest.in");
-    std::ifstream mergeOut("mergeTest.out");
-    std::string line;
-    int cases = 1;
-    while(std::getline(mergeIn, line)) {
-        std::string outline;
-        std::getline(mergeOut, outline);
-        std::string a1 = line.substr(0,line.find(","));
-        std::string a2 = line.substr(line.find(",")+1);
-        //std::cout << a1 << std::endl;
-        //std::cout << a2 << std::endl;
-        int length1;
-        std::stringstream stream1(a1);
-        stream1 >> length1;
-        int length2;
-        std::stringstream stream2(a2);
-        stream2 >> length2;
+bool testMerge(int n, int t) {
 
-        int *array = new int[length1+length2];
-        for(int i = 0; i < length1; i++) {
-            stream1 >> array[i];
-        }
-        for(int i = 0; i < length2; i++) {
-            stream2 >> array[length1+i];
-        }
+    std::random_device sd;
+    std::mt19937 generator(sd());
+    std::uniform_int_distribution<int> distribution(n/20, n - (n/20));
 
-        int outlength = length1 + length2;
-        std::stringstream outstream(outline);
-        int *outarray = new int[outlength];
-        for(int i = 0; i < outlength; i++) {
-            outstream >> outarray[i];
-        }
+    int mid = distribution(generator);
 
-        merge(array, outlength, length1);
+    int* A = gen_list_unsorted(n);
+    std::sort(A, A + mid);
+    std::sort(A + mid, A + n);
 
-        for(int i = 0; i < outlength; i++) {
-            if(array[i] != outarray[i]) {
-                std::cout << "Error on test case: " << cases << std::endl;
-                std::cout << "Your Array: ";
-                for(int i = 0; i < length1; i++) {
-                    std::cout << array[i] << " ";
-                }
-                std::cout << std::endl;
-                std::cout << "Target Array: ";
-                for(int i = 0; i < outlength; i++) {
-                    std::cout << outarray[i] << " ";
-                }
-                std::cout << std::endl;
-                return;
-            }
-        }
-        cases++;
+    int* B = new int[n];
+    std::memcpy(B, A, sizeof(int)*n);
+    std::sort(B, B + n);
 
-    }
-}
+    merge(A, n, mid);
 
-void testMergeSort(int n) {
-    //Test the mergeSort function
-    std::ifstream sortIn("sortTest.in");
-    std::ifstream sortOut("sortTest.out");
-    std::string line;
-    int cases = 1;
-    while(std::getline(sortIn, line)) {
-        std::string outline;
-        std::getline(sortOut, outline);
-        std::stringstream stream(line);
-        int n;
-        stream >> n;
-        int *array = new int[n];
-        for(int i = 0; i < n; i++) {
-            stream >> array[i];
-        }
-        int *sortedarray = new int[n];
-        std::stringstream stream2(outline);
-        for(int i = 0; i < n; i++) {
-            stream2 >> sortedarray[i];
-        }
+    bool success = compareArrays(A, B, n);
 
-        MergeSort(array, n);
-
-        for(int i = 0; i < n; i++) {
-            if(array[i] != sortedarray[i]) {
-                std::cout << "Error on test case: " << cases << std::endl;
-                std::cout << "Your Array: ";
-                for(int i = 0; i < n; i++) {
-                    std::cout << array[i] << " ";
-                }
-                std::cout << std::endl;
-                std::cout << "Target Array: ";
-                for(int i = 0; i < n; i++) {
-                    std::cout << sortedarray[i] << " ";
-                }
-                std::cout << std::endl;
-                return;
-            }
-        }
-        cases++;
+    if (success) {
+        std::cout << "Merge successful on trial " << t << std::endl;
+        return true;
+    } else {
+        std::cout << "Merge failed on trial " << t << std::endl;
+        return false;
     }
 
 }
 
-raceMergeSorts(int n) {
+bool testMergeSort(int n, int t) {
+
+    int* A = gen_list_unsorted(n);
+    int* B = new int[n];
+    std::memcpy(B, A, sizeof(int)*n);
+    std::sort(B, B + n);
+
+    MergeSort(A, n);
+
+    bool success = compareArrays(A, B, n);
+
+    if (success) {
+        std::cout << "MergeSort successful on trial " << t << std::endl;
+        return true;
+    } else {
+        std::cout << "MergeSort failed on trial " << t << std::endl;
+        return false;
+    }
+
+}
+
+void raceMergeSorts(int n) {
 
 }
 
 // Generate a random, unsorted list
-int* gen_list_unsorted(unsigned int size) {
+int* gen_list_unsorted(int size) {
 
     std::random_device sd;
     std::mt19937 generator(sd());
@@ -181,7 +107,8 @@ int main(int argc, char** argv) {
         std::cerr << "Invalid Arguments. Usage: test_type(str) input_size(int)" << std::endl;
         return 1;
     }
-    char test_types[4][11] = {"merge", "merge_sort", "ms_vs_msp", "custom"};
+    int num_ttypes = 4;
+    char test_types[num_ttypes][11] = {"merge", "merge_sort", "ms_vs_msp", "custom"};
     bool valid = false;
     for (int i = 0; i < 4; i++) {
         if (strcmp(argv[1], test_types[i])) {
@@ -191,13 +118,13 @@ int main(int argc, char** argv) {
     }
     if (!valid) {
         std::cerr << "Invalid Arguments. Possibilities for test_type are:" << std::endl;
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < num_ttypes; i++) {
             std::cout << test_types[i] << std::endl;
         }
         return 2;
     }
     if (atoi(argv[2]) <= 0) {
-        std::cerr << "Invalid Arguments. Please enter a valid number n > 0."
+        std::cerr << "Invalid Arguments. Please enter a valid number n > 0.";
         return 3;
     }
 
@@ -205,11 +132,27 @@ int main(int argc, char** argv) {
     int input_size = atoi(argv[2]);
     if (!strcmp(argv[1], test_types[0])) {
 
-        testMerge(input_size);
+        bool success = true;
+        for (int i = 0; i < 3; i++) {
+            success = success && testMerge(input_size, i+1);
+        }
+        if (success) {
+            std::cout << "Merge successful for input size " << input_size << std::endl;
+        } else {
+            std::cout << "Merge failed for input size " << input_size << std::endl;
+        }
 
     } else if (!strcmp(argv[1], test_types[1])) {
 
-        testMergeSort(input_size);
+        bool success = true;
+        for (int i = 0; i < 3; i++) {
+            success = success && testMergeSort(input_size, i+1);
+        }
+        if (success) {
+            std::cout << "MergeSort successful for input size " << input_size << std::endl;
+        } else {
+            std::cout << "MergeSort failed for input size " << input_size << std::endl;
+        }
 
     } else if (!strcmp(argv[1], test_types[2])) {
 
