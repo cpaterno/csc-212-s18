a7b250aba47c56b7e3629523ea092ea3971bc584
davinbernier <davinlbernier@gmail.com>
2018-03-02 10:53:46 -0500

    Update main.cpp

--- lab-07/main.cpp
+++ lab-07/main.cpp
@@ -2,37 +2,56 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <random>
+#include <chrono>
 
-void testMergeSort();
-void testMerge();
+void testMerge(int n);
+void testMergeSort(int n);
+void raceMergeSorts(int n);
 void merge(int* A, int n, int mid);
 void MergeSort(int* A, int n);
 
-
-int main() {
-
-    //Uncomment the following lines to test merge and MergeSort respectively
-    //testMerge();
-    //testMergeSort();
-
-}
-
 void merge(int* A, int n, int mid) {
-    /*
-        Implement the merge function here
-    */
+    int* B = new int[n];
+    for (int i = 0; i < n; i++) {
+        B[i] = A[i];
+    }
+    int x = 0;
+    int y = mid;
+    int idx = 0;
+    while (x < mid && y < n) {
+        if (B[x] < B[y]) {
+            A[idx++] = B[x++];
+        } else {
+            A[idx++] = B[y++];
+        }
+    }
+
+    if (x < mid) {
+        y = x;
+        n = mid;
+    }
+
+    for (int i = y; i < n; i++) {
+        A[idx++] = B[i];
+    }
+
+    delete [] B;
 }
 
 void MergeSort(int* A, int n) {
-    /*
-        Implement the MergeSort function here
-    */
+    if (n != 1) {
+        MergeSort(A, n/2);
+        int n2 = (n % 2 == 0) ? n/2 : n/2 + 1;
+        MergeSort(A + n/2, n2);
+        merge(A, n, n/2);
+    }
 }
 
-void ImprovedMergeSort(int* A, int n) {
-    /*
-        Implement the ImprovedMergeSort function here
-    */
+void customTest(int n) {
+
+    /* You can write your own additional tests here if you would like */
+
 }
 
 void testMerge() {
@@ -46,8 +65,8 @@ void testMerge() {
         std::getline(mergeOut, outline);
         std::string a1 = line.substr(0,line.find(","));
         std::string a2 = line.substr(line.find(",")+1);
-        std::cout << a1 << std::endl;
-        std::cout << a2 << std::endl;
+        //std::cout << a1 << std::endl;
+        //std::cout << a2 << std::endl;
         int length1;
         std::stringstream stream1(a1);
         stream1 >> length1;
@@ -93,7 +112,7 @@ void testMerge() {
     }
 }
 
-void testMergeSort() {
+void testMergeSort(int n) {
     //Test the mergeSort function
     std::ifstream sortIn("sortTest.in");
     std::ifstream sortOut("sortTest.out");
@@ -135,8 +154,15 @@ void testMergeSort() {
         }
         cases++;
     }
+
+}
+
+raceMergeSorts(int n) {
+
+}
+
 // Generate a random, unsorted list
-int* gen_list_unsorted(int size) {
+int* gen_list_unsorted(unsigned int size) {
 
     std::random_device sd;
     std::mt19937 generator(sd());
@@ -146,4 +172,53 @@ int* gen_list_unsorted(int size) {
         nlist[i] = distribution(generator);
     }
     return nlist;
+}
+
+int main(int argc, char** argv) {
+
+    /* Input validation */
+    if (argc != 3) {
+        std::cerr << "Invalid Arguments. Usage: test_type(str) input_size(int)" << std::endl;
+        return 1;
+    }
+    char test_types[4][11] = {"merge", "merge_sort", "ms_vs_msp", "custom"};
+    bool valid = false;
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(argv[1], test_types[i])) {
+            valid = true;
+            break;
+        }
+    }
+    if (!valid) {
+        std::cerr << "Invalid Arguments. Possibilities for test_type are:" << std::endl;
+        for (int i = 0; i < 4; i++) {
+            std::cout << test_types[i] << std::endl;
+        }
+        return 2;
+    }
+    if (atoi(argv[2]) <= 0) {
+        std::cerr << "Invalid Arguments. Please enter a valid number n > 0."
+        return 3;
+    }
+
+
+    int input_size = atoi(argv[2]);
+    if (!strcmp(argv[1], test_types[0])) {
+
+        testMerge(input_size);
+
+    } else if (!strcmp(argv[1], test_types[1])) {
+
+        testMergeSort(input_size);
+
+    } else if (!strcmp(argv[1], test_types[2])) {
+
+        raceMergeSorts(input_size);
+
+    } else if (!strcmp(argv[1], test_types[3])) {
+
+        customTest(input_size);
+
+    }
+    return 0;
 }
