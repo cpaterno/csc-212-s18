319963b16818838023303ee5d597e53b12251669
davinbernier <davinlbernier@gmail.com>
2018-03-09 10:47:16 -0500

    Create darray.cc
    Let me know which methods should be added and which should be removed.

---
+++ lab-08/darray.cc
@@ -0,0 +1,84 @@
+#include "darray.h"
+
+/* NOTE: The comments in this file are temporary
+ *       and will be deleted before the lab goes
+ *       live.
+ */
+
+/* Constructors */
+DArray::DArray() {
+
+}
+
+DArray::DArray(int c) {
+
+}
+
+/* Optional Constructor */
+DArray::DArray(int* a, int n) {
+
+}
+
+/* Destructor */
+DArray::~DArray() {
+
+}
+
+/* Private Helper Functions */
+void DArray::grow() {
+
+}
+
+void DArray::shrink() {
+
+}
+
+bool DArray::index_in_bounds(int idx) {
+
+}
+
+/* Simple Accessor Methods */
+int DArray::size() {
+
+}
+
+int DArray::capacity() {
+
+}
+
+int DArray::at(int idx) {
+
+}
+
+int DArray::front() {
+
+}
+
+int DArray::back() {
+
+}
+
+bool DArray::empty() {
+
+}
+
+/* More Complex Methods */
+void DArray::set(int elem, int idx) {
+
+}
+
+void DArray::push_back(int elem) {
+
+}
+
+int DArray::pop_back() {
+
+}
+
+void DArray::insert(int elem, int idx) {
+
+}
+
+void DArray::erase(int idx) {
+
+}
