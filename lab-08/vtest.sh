80468f3a9314bbe87096a5b3753392f0f01e85b2
davinbernier <davinlbernier@gmail.com>
2018-03-19 16:18:31 -0400

    Fix filenames and execution
    Was darraySol.cc but needed to be darray.cc
    Also, while not so much of a fix as a modification, line 9 now prints one less unnecessary line.

--- lab-08/vtest.sh
+++ lab-08/vtest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-g++ -g -std=c++11 darraySol.cc main.cc -o lab8
+g++ -g -std=c++11 darray.cc main.cc -o lab8
 
 counter=1
 while [ $counter -le 11 ]
 do
     echo $counter
-    ./lab8 $counter
+    ./lab8 $counter | grep .
     ((counter++))
 done
