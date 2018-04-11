5bce77b5a810c776f9e2ad3509c640c9784674e7
Alex Lachance <9153879+LachanceAlex@users.noreply.github.com>
2018-04-11 11:56:07 -0400

    Update islands.cc

--- lab-10/islands.cc
+++ lab-10/islands.cc
@@ -1,30 +1,26 @@
 #include <iostream>
 
-int count_islands(int* sequence, int start, int end) {
-    /*
-     *  Your solution here
-     */
-}
-
 int main() {
 
+    // read number of trials
     int trials;
     std::cin >> trials;
 
-    int* island_counts = new int[trials];
+    // loop over all trials
+    int myid;
+    int sequence[12];
     for (int i = 0; i < trials; i++) {
-        int* sequence = new int[12];
-        std::cin >> sequence[0];
+        // read input
+        std::cin >> myid;
         for (int j = 0; j < 12; j++) {
             std::cin >> sequence[j];
         }
-        island_counts[i] = count_islands(sequence, 0, 11);
-        delete [] sequence;
+        
+        // do your work here
+        // TODO
+        
+        // print output
+        // TODO
     }
-
-    for (int i = 0; i < trials; i++) {
-        std::cout << i+1 << ' ' << island_counts[i] << std::endl;
-    }
-    delete [] island_counts;
 
 }
