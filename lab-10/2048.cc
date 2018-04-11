e51b8ec3b5f1c097237b0b334bc619fcb0800647
Alex Lachance <9153879+LachanceAlex@users.noreply.github.com>
2018-04-11 11:45:20 -0400

    Update 2048.cc

--- lab-10/2048.cc
+++ lab-10/2048.cc
@@ -8,9 +8,9 @@ int** _2048(int** board, int move) {
 
 int main() {
 
-    int** board = new int*[4];
+    // read input
+    int board[4][4];
     for (int i = 0; i < 4; i++) {
-        board[i] = new int[4];
         for (int j = 0; j < 4; j++) {
             std::cin >> board[i][j];
         }
@@ -18,9 +18,12 @@ int main() {
     int move;
     std::cin >> move;
 
-    int** moved_board;
-    moved_board = _2048(board, move);
-
+    
+    // TODO your solution goes here
+    int moved_board[4][4];
+    // TODO
+    
+    // write output
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             std::cout << moved_board[i][j];
@@ -28,11 +31,4 @@ int main() {
         }
         std::cout << std::endl;
     }
-    for (int i = 0; i < 4; i++) {
-        delete [] board[i];
-        delete [] moved_board[i];
-    }
-    delete [] board;
-    delete [] moved_board;
-
 }
