b33cb6a0574e19350c9a436fa7b26d7bfc1d0d45
Alex Lachance <9153879+LachanceAlex@users.noreply.github.com>
2018-04-11 11:51:20 -0400

    Update odd_gnome.cc

--- lab-10/odd_gnome.cc
+++ lab-10/odd_gnome.cc
@@ -1,32 +1,31 @@
 #include <iostream>
 
-int* odd_gnome(int num_groups, int* gs_in_each, int** groups) {
-    /*
-     *  Your solution here
-     */
-}
-
 int main(int argc, char** argv) {
-
+    // read number of cases (groups)
     int num_groups;
     std::cin >> num_groups;
-    int** groups = new int*[num_groups];
-    int* gs_in_each = new int[num_groups];
+    
+    // loop to process each case
     for (int i = 0; i < num_groups; i++) {
+        
+        // read number of gnomes
         int num_gnomes;
         std::cin >> num_gnomes;
-        gs_in_each[i] = num_gnomes;
+        
+        // read gnomes
         int* gnomes = new int[num_gnomes];
         for (int j = 0; j < num_gnomes; j++) {
             std::cin >> gnomes[j];
         }
-        groups[i] = gnomes;
-    }
-
-    int* result = odd_gnome(num_groups, gs_in_each, groups);
-
-    for (int i = 0; i < num_groups; i++) {
-        std::cout << result[i] << std::endl;
+        
+        // do your work here 
+        // TODO: process this case
+        // ...
+        
+        // TODO print the result here
+        
+        // delete memory
+        delete [] gnomes;
     }
 
     return 0;
