b3703f1e54e8e7a8b078ecd83da7c59c4aef62d3
Marco <malvarezvg@gmail.com>
2018-04-11 12:08:30 -0400

    Update mancala.cc

--- lab-10/mancala.cc
+++ lab-10/mancala.cc
@@ -1,38 +1,28 @@
 #include <iostream>
 
-int* mancala(int count, int* boxes_filled) {
-    /*
-     *  Your solution here
-     */
-}
-
 int main() {
 
+    // read number of datasets
     int num_trials;
     std::cin >> num_trials;
-
-    int* trials = new int[num_trials];
+    
+    
+    int k;
+    int count;
+    
+    // loop over all datasets
     for (int i = 0; i < num_trials; i++) {
-        std::cin >> trials[i];
-        std::cin >> trials[i];
-    }
 
-    int** results = new int*[num_trials];
-    int* results_sizes = new int[num_trials];
-    for (int i = 0; i < num_trials; i++) {
-        results[i] = mancala(trials[i], results_sizes+i);
-    }
+        // read k and count
+        std::cin >> k;
+        std::cin >> count;
+        
+        // TODO
+        // your work goes here
+        
+        // TODO
+        // print output for this case
 
-    for (int i = 0; i < num_trials; i++) {
-        std::cout << i + 1 << ' ' << results_sizes[i] << std::endl;
-        for (int j = 0; j < results_sizes[i]; j++) {
-            std::cout << results[i][j];
-            if (j % 10 == 9 || j == results_sizes[i]) {
-                std::cout << std::endl;
-            } else {
-                std::cout << ' ';
-            }
-        }
     }
 
     return 0;
